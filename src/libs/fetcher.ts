1e5e68e84995509ecd72880fb8a67bebfa428e5d
cop <32881179+cop-discord@users.noreply.github.com>
2024-09-09 14:02:41 -0700

    Update fetcher.ts

--- src/libs/fetcher.ts
+++ src/libs/fetcher.ts
@@ -2,7 +2,5 @@ import axios from "axios";
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
-const fetch = (url: string) => axios.get(url).then((res) => res);
-
 export default fetcher;
 
